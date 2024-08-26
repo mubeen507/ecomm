f63adbd53856127691ec734fe4e0eea04236cd17
Kavya254 <kavyalearning04@gmail.com>
2024-08-26 15:44:01 +0000

    Create Deployment Script

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ sudo rm -rf /var/www/html
 sleep 5
 
 echo "Clone Login App"
-sudo git clone https://github.com/ravi2krishna/ecomm.git /var/www/html
+sudo git clone https://github.com/Kavya254/ecomm.git /var/www/html
 sleep 5
 
 echo "Browse Login App with Server Public IP"
